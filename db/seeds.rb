b2ea7c09813a4145446d0527eb2fb42afafaf78e
Leon <kliwaru@gmail.com>
2019-02-20 19:44:37 +0800

    seed complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,26 @@ ActiveRecord::Base.connection.reset_pk_sequence!(:comments);
 ActiveRecord::Base.connection.reset_pk_sequence!(:photos);
 ActiveRecord::Base.connection.reset_pk_sequence!(:users);
 
+# clear user avatar directory
+dir_path = "public/uploads/user/avatar/*"
+Dir.glob(dir_path).each do |d|
+    fn = File.join(d, "*")
+    Dir.glob(fn).each do |f|
+	    p f
+        File.delete(f)
+	end
+end
+
+# clear photo url directory
+dir_path = "public/uploads/photo/url/*"
+Dir.glob(dir_path).each do |d|
+    fn = File.join(d, "*")
+    Dir.glob(fn).each do |f|
+	    p f
+        File.delete(f)
+	end
+end
+
 User.create(
     email: "admin@mail.com",
     password: "123",
@@ -21,7 +41,7 @@ User.create(
 )
 
 i = 0
-3.times do 
+30.times do 
     user = User.new(
         email: "user#{i}@mail.com",
         password: "123",
@@ -30,9 +50,48 @@ i = 0
         address: Faker::Address.street_address,
         long: rand(0..100),
         lat: rand(0..60),
-        avatar = open(Dir.glob("db/seeds/images/*").sample)
+        avatar: open(Dir.glob("db/seeds/images/*").sample),
     )
     i += 1
     p "#{user.username} created successfully" if user.save
 end
 
+
+i = 0
+80.times do 
+    photo = Photo.new(
+        url: open(Dir.glob("db/seeds/images/*").sample),
+        caption: Faker::TvShows::GameOfThrones.quote,
+        user_id: rand(2..30),
+        tags: [],
+    )
+
+    tag_ar = ['cat', 'cute', 'dog', 'puppy', 'adorable']
+    rand(1..5).times {
+        photo.tags << tag_ar.sample
+    }
+
+    i += 1
+    p "#{photo.caption} created successfully" if photo.save
+end
+
+i = 0
+200.times do
+    comment = Comment.new(
+        comment: Faker::TvShows::GameOfThrones.quote,
+        user_id: rand(2..30),
+        photo_id: rand(1..100),
+    )
+    i += 1
+    p "Comment ##{i} created successfully" if comment.save
+end
+
+i = 0
+150.times do
+    like = Like.new(
+        user_id: rand(2..30),
+        photo_id: rand(1..100),
+    )
+    i += 1
+    p "Like ##{i} created successfully" if like.save
+end
