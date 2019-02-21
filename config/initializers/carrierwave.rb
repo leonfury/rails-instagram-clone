ae52cb8580bea00c1508b0cb0f1ac318382a51be
Leon <kliwaru@gmail.com>
2019-02-21 17:12:54 +0800

    x

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,3 +1,4 @@
+
 CarrierWave.configure do |config|
         
     config.fog_provider = 'fog/aws'                        # required
@@ -14,3 +15,4 @@ CarrierWave.configure do |config|
     config.fog_public     = false                                                 # optional, defaults to true
     # config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
 end
+
